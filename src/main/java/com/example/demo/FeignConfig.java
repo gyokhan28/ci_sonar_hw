780ac60c09a404536025c8c715c08901aa830db3
Gyokhan <gyokhan28@gmail.com>
2024-06-26 20:00:35 +0300

    reduced medium issues v3

--- src/main/java/com/example/demo/FeignConfig.java
+++ src/main/java/com/example/demo/FeignConfig.java
@@ -9,11 +9,13 @@ import org.springframework.context.annotation.Configuration;
 public class FeignConfig {
     @Bean
     public RequestInterceptor requestInterceptor() {
-        return requestTemplate -> {
-           requestTemplate.header("Authorization",
-                   "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOGZiMzY5YTYyOWRmN2JkMjkwY2U1YzU5ODc2OWFjOSIsInN1YiI6IjY2NjQ1ZjJhY2Y1MmFkYjFhY2UwYjI2MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.V08SO1kJe-jYUxeKP0wm9AnQsaxzJXgdYmroSO8maGI");};
+        return requestTemplate ->
+                requestTemplate.header("Authorization",
+                        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOGZiMzY5YTYyOWRmN2JkMjkwY2U1YzU5ODc2OWFjOSIsInN1YiI6IjY2NjQ1ZjJhY2Y1MmFkYjFhY2UwYjI2MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.V08SO1kJe-jYUxeKP0wm9AnQsaxzJXgdYmroSO8maGI");
     }
-    @Bean public ErrorDecoder errorDecoder() {
-        return new CustomErrorDecoder(); }
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return new CustomErrorDecoder();
+    }
 
 }
